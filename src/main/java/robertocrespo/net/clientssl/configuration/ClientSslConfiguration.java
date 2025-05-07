a6340f84c575befcfec9ed6deffbcb6d2a004ba8
billy___idle <billy___idle@hotmail.com>
2025-05-07 15:31:17 -0500

    Se migra a spring boot v3.2.12

--- src/main/java/robertocrespo/net/clientssl/configuration/ClientSslConfiguration.java
+++ src/main/java/robertocrespo/net/clientssl/configuration/ClientSslConfiguration.java
@@ -1,10 +1,11 @@
 package robertocrespo.net.clientssl.configuration;
 
-import javax.net.ssl.SSLContext;
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,33 +13,25 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+
 @Configuration
 public class ClientSslConfiguration {
 
+    String protocol = "TLSv1.2";
     @Value("${client.ssl.key-store}")
     private Resource trustStore;
     @Value("${client.ssl.key-store-password}")
     private String trustStorePassword;
 
-    String protocol = "TLSv1.2";
-
-
     @Bean
     RestTemplate restTemplate() throws Exception {
-        SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(
-                        trustStore.getURL(),
-                        trustStorePassword.toCharArray()
-                ).build();
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
 
-        SSLConnectionSocketFactory socketFactory =
-                new SSLConnectionSocketFactory(sslContext);
-
-        HttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(socketFactory).build();
-
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory(httpClient);
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().setSSLSocketFactory(socketFactory).build();
+        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
         return new RestTemplate(factory);
     }
--- src/main/java/robertocrespo/net/clientssl/controller/ClientSslController.java
+++ src/main/java/robertocrespo/net/clientssl/controller/ClientSslController.java
@@ -1,6 +1,5 @@
 package robertocrespo.net.clientssl.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ public class ClientSslController {
 
     private ClientSslService sslService;
 
-    @Autowired
     public ClientSslController(ClientSslService sslService) {
 
         this.sslService = sslService;
